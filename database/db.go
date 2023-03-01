0a00b0bc60dde822145f81b0e14a51ebab7dd43b
xsully12 <sullivan.camargo@fatec.sp.gov.br>
2023-03-01 11:54:52 -0300

    difinindo as variaveis

--- database/db.go
+++ database/db.go
@@ -15,11 +15,11 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	port := os.Getenv("DB_PORT")
+	host := os.Getenv("HOST")
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	dbname := os.Getenv("DBNAME")
+	port := os.Getenv("DBPORT")
 
 	stringDeConexao := "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port + " sslmode=disable"
 
